f86b954b7f225ea78dcabe161c0e73f6683315cb
Oliver Zhang <oliverzh2000@gmail.com>
2021-01-22 00:00:23 -0500

    Upgrade to gl 4.3

--- graphics/shaders/quad_renderer.fs
+++ graphics/shaders/quad_renderer.fs
@@ -1,12 +1,13 @@
-#version 330 core
+#version 430 core
 out vec4 FragColor;
 
 in vec2 TexCoord;
 
 // texture sampler
 uniform sampler2D texture1;
+uniform float samples_per_pixel;
 
 void main()
 {
-	FragColor = texture(texture1, TexCoord);
+	FragColor = texture(texture1, TexCoord) / samples_per_pixel;
 }
